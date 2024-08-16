267694e698afbd0355ee1890cff82dc0a7e0591a
KvaAan <korzhenevskiy05@yandex.ru>
2024-08-16 23:48:51 +0400

    edit register-connectors.sh

--- scripts/debezium/register-connectors.sh
+++ scripts/debezium/register-connectors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -X PUT -H "Content-Type: application/json" -d @reservation-service/reservation-outbox-connector.json http://localhost:8083/connectors/reservation-outbox-connector/config
-curl -X PUT -H "Content-Type: application/json" -d @hotel-service/hotel-outbox-connector.json http://localhost:8083/connectors/hotel-outbox-connector/config
-curl -X PUT -H "Content-Type: application/json" -d @payment-service/payment-outbox-connector.json http://localhost:8083/connectors/payment-outbox-connector/config
+curl -X PUT -H "Content-Type: application/json" -d @debezium/connectors/reservation-outbox-connector.json http://localhost:8083/connectors/reservation-outbox-connector/config
+curl -X PUT -H "Content-Type: application/json" -d @debezium/connectors/hotel-outbox-connector.json http://localhost:8083/connectors/hotel-outbox-connector/config
+curl -X PUT -H "Content-Type: application/json" -d @debezium/connectors/payment-outbox-connector.json http://localhost:8083/connectors/payment-outbox-connector/config
 
